d8c8050523890f14577282b75cdb38e594f8796a
Aadarsh Thakur <aadarshthakur80501@gmail.com>
2022-09-03 13:01:05 +0530

    postman

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import Cards from './dbCards.js'
+import Cors from 'cors'
 
 //App Config
 const app = express()
@@ -8,6 +9,8 @@ const port = process.env.PORT || 8001
 const connection_url = `mongodb+srv://admin:DOTjkLpHLOOmmaA6@cluster0.14fkuvg.mongodb.net/?retryWrites=true&w=majority `
 
 // Middlewares
+app.use(express.json())
+app.use(Cors())
 
 //DB config
 mongoose.connect(connection_url, {
